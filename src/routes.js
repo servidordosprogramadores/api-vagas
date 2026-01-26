8df71ac1a670b32b58b4b745bb5a421eec95916b
matheusaudibert <matheusaudibert2019@outlook.com>
2026-01-26 20:03:31 -0300

    feat(route): default route added

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,13 @@ const JobController = require('./controllers/JobController');
 
 const routes = express.Router();
 
+routes.get('/', (req, res) => {
+  return res.json({
+    rotas_disponiveis: ['/estagio', '/pleno', '/senior'],
+    repositorio: 'https://github.com/matheusaudibert/jobs-api'
+  });
+});
+
 routes.get('/estagio', JobController.getEstagio);
 routes.get('/pleno', JobController.getPleno);
 routes.get('/senior', JobController.getSenior);
