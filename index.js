ba3fa6b00883f1190ea665006e77e41acd911710
Matheus Audibert <85813476+matheusaudibert@users.noreply.github.com>
2026-06-21 20:51:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-const PORT = 2400;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado em http://localhost:${PORT}`);
